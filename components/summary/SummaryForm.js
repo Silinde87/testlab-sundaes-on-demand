4804e5f9e8ee9c7197b8191ee57fdca58f8289f7
Silinde87 <paurodriguezmolina@gmail.com>
2021-06-17 11:30:47 +0200

    implement checbox functionality

--- components/summary/SummaryForm.js
+++ components/summary/SummaryForm.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
 export default function SummaryForm() {
-    return (
-        <div>
-            
-        </div>
-    )
+	const [tcChecked, setTcChecked] = useState(false);
+
+	const checkboxLabel = (
+		<span>
+			I agree to <span style={{ color: 'blue' }}> Terms and Conditions</span>
+		</span>
+	);
+
+	return (
+		<form>
+			<div className="form-check mb-3">
+				<input
+					id="check-terms"
+					className="form-check-input"
+					type="checkbox"
+					defaultChecked={tcChecked}
+					aria-checked={tcChecked}
+					onChange={(e) => setTcChecked(e.target.checked)}
+				/>
+				<label htmlFor="check-terms" className="form-check-label">
+					{checkboxLabel}
+				</label>
+			</div>
+			<Button variant="primary" type="submit" disabled={!tcChecked}>
+				Confirm order
+			</Button>
+		</form>
+	);
 }
--- components/summary/test/SummaryForm.test.js
+++ components/summary/test/SummaryForm.test.js
@@ -1,28 +1,33 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import SummaryForm from './../SummaryForm';
 
-test('checbox has correct initial state', () => {
-	render(<SummaryForm />);
+describe('Checkbox functionality', () => {
+	test('Initial state', () => {
+		render(<SummaryForm />);
+		const checkbox = screen.getByRole('checkbox', { name: /terms and conditions/i });
+		const confirmButton = screen.getByRole('button', { name: /confirm order/i });
 
-	const checkbox = screen.getByRole('checkbox', { name: 'I agree to Tems and Conditions' });
-	expect(checkbox).not.toBeChecked();
-});
+        //check initial state of checkbox and button
+		expect(checkbox).not.toBeChecked();
+		expect(confirmButton).toBeDisabled();
+	});
 
-test('checkbox enables button when checked and disables button when unchecked', () => {
-	render(<SummaryForm />);
+	test('checkbox enables button when checked and disables button when unchecked', () => {
+		render(<SummaryForm />);
 
-	const checkbox = screen.getByRole('checkbox', { name: 'I agree to Tems and Conditions' });
-	const confirmButton = screen.getByRole('button', { name: 'Confirm order' });
+		const checkbox = screen.getByRole('checkbox', { name: /terms and conditions/i });
+		const confirmButton = screen.getByRole('button', { name: 'Confirm order' });
 
-    //check initial state of checkbox and button
-    expect(checkbox).not.toBeChecked();
-    expect(confirmButton).toBeDisabled();
+		//check initial state of checkbox and button
+		expect(checkbox).not.toBeChecked();
+		expect(confirmButton).toBeDisabled();
 
-    //Checks button is enable when click on checkbox
-    fireEvent.click(checkbox);
-    expect(confirmButton).toBeEnabled();
+		//Checks button is enable when click on checkbox
+		fireEvent.click(checkbox);
+		expect(confirmButton).toBeEnabled();
 
-    //Checks button is disabled when click on checkbox again
-    fireEvent.click(checkbox);
-    expect(confirmButton).toBeDisabled();
+		//Checks button is disabled when click on checkbox again
+		fireEvent.click(checkbox);
+		expect(confirmButton).toBeDisabled();
+	});
 });
