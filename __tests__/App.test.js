29af67cf3370fda1fe4991b1356732447aee44fe
Silinde87 <paurodriguezmolina@gmail.com>
2021-06-17 10:08:42 +0200

    Initial commit and bootstrap setup

---
+++ __tests__/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/index';
+
+test('button has correct initial color', () => {
+	// render(<Home />);
+	// //find an element with role of button and text of 'Change to MidnightBlue';
+	// const colorButton = screen.getByRole('button', { name: 'Change to Midnight Blue' });
+});
