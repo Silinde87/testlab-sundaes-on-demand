50c5d8eca5274e1756c11d3b5824ba5b4fb6a477
Silinde87 <paurodriguezmolina@gmail.com>
2021-06-17 13:29:14 +0200

    Mock Service Worker setup

--- setupTests.js
+++ setupTests.js
@@ -3,4 +3,17 @@
 
 // used for __tests__/testing-library.js
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
+
+// Mock service workers setup
+import { server } from './mocks/server.js';
+
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+
+// Resets any request handlers that we may add during tests.
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished
+afterAll(() => server.close());
+
