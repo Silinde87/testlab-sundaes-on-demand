ef688a8264bbc449fc50894c509de7f0b6da8463
Silinde87 <paurodriguezmolina@gmail.com>
2021-06-20 13:03:11 +0200

    Create Alert tests and component. Update RTL/react

--- components/entry/Options.js
+++ components/entry/Options.js
@@ -3,19 +3,21 @@ import axios from 'axios';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
 import { Row } from 'react-bootstrap';
+import AlertBanner from '../common/AlertBanner';
 
 export default function options({ optionType }) {
 	const [items, setItems] = useState([]);
+	const [error, setError] = useState(false);
 
 	// optionType is 'scoops' or 'toppings'
 	useEffect(() => {
 		axios
 			.get(`http://localhost:3030/${optionType}`)
 			.then((response) => setItems(response.data))
-			.catch((err) => {
-				// TODO: handle error response
-			});
+			.catch((err) => setError(true));
 	}, [optionType]);
+
+	if (error) return <AlertBanner />;
 
 	const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
